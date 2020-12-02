fc438de8e9572e8b355a249ad7fffc75cabfc372
y0sh1DE <Joshua.Martius@RWTH-Aachen.de>
2020-12-02 21:33:24 +0100

    Rewrite with json usage

--- listener.py
+++ listener.py
@@ -2,13 +2,12 @@ import os
 import os.path
 from phue import Bridge
 import time
+import json
 
-TOKENNAME = "./.athome"
-HOSTNAME = "137.226.214.206"
-SLEEPTIME = 10
-groupsToTurnOn = ["Wohnzimmer"]
-hue = Bridge("137.226.214.9")
+config = json.loads(open("./config.json","r").read())
+hue = Bridge(config["bridgeAddress"])
 hue.connect()
+
 
 def sendHeartbeat():
     for i in range(2):
@@ -26,40 +25,42 @@ def turnOffLights():
 
 def turnOnLights():
     for group in hue.groups:
-        if group.name in groupsToTurnOn:
+        if group.name in config["groupsToTurnOn"]:
             group.on = True
     return
 
 def setToken():
-    open(TOKENNAME,'a').close()
+    config["atHome"] = True
     return
 
 def removeToken():
-    os.remove(TOKENNAME)
+    config["atHome"] = False
     return
 
-def checkPing(hostname):
-    if os.system("ping -q -c 1 " + hostname) == 0:
-        return True
-    else:
-        return False
-
-while True:
-    athome = checkPing(HOSTNAME)
-    if os.path.isfile(TOKENNAME): # if should be athome
-        if athome:
-            print("Should be Home: Yes | Is at Home: Yes")
-        else:
-            print("Should be Home: Yes | Is at Home: No")
-            turnOffLights()
-            removeToken()
-            print("Turned off lights. Sleeping.")
-    else: # if shouldnt be at home
-        if athome:
-            print("Should be Home: No | Is at Home: Yes")
-            turnOnLights()
-            setToken()
-            print("Turned on lights and set token.")
-        else: # shouldnt be at home and isnt
-            print("Should be Home: No | Is at Home: No")
-    time.sleep(SLEEPTIME)
+def checkPing(hostnames):
+    for host in hostnames:    
+        if os.system("ping -q -c 1 " + host) == 0:
+            return True
+try:
+    while True:
+        athome = checkPing(config["devices"])
+        if config["atHome"]: # if should be athome
+            if athome:
+                print("Should be Home: Yes | Is at Home: Yes")
+            else:
+                print("Should be Home: Yes | Is at Home: No")
+                turnOffLights()
+                removeToken()
+                print("Turned off lights. Sleeping.")
+        else: # if shouldnt be at home
+            if athome:
+                print("Should be Home: No | Is at Home: Yes")
+                turnOnLights()
+                setToken()
+                print("Turned on lights and set token.")
+            else: # shouldnt be at home and isnt
+                print("Should be Home: No | Is at Home: No")
+        time.sleep(config["sleeptime"])
+except KeyboardInterrupt:
+    json.dump(config, open("./config.json","w"))
+    print("Saved current config state")
